a1c56e8f93ad66cbd127adcbe83f9ddafe82f3e6
MusaKolcuk <musa_kolcuk_1999@hotmail.com>
2023-05-23 00:21:10 +0300

    commit

--- helpers/database/connectDatabase.js
+++ helpers/database/connectDatabase.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const connectDatabase = () => {
 
     mongoose.connect(process.env.MONGO_URI, {
-        dbName : "api-mimari",
+        dbName : "todogether",
         useNewUrlParser: true,
         useUnifiedTopology: true,                                                   //performansi arttirir.
 
